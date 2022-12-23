8968552240d093f166ae8c0e6b3c27a4541ba16a
ArkhadeNikita <121220173+ArkhadeNikita@users.noreply.github.com>
2022-12-23 10:23:23 +0530

    Add files via upload

---
+++ FourthAss.java
@@ -0,0 +1,61 @@
+package labAssignment;
+
+import java.util.Scanner;
+
+interface Printable     // interface 
+{
+	public void print();   // abstract method
+}
+
+interface Calculate
+{
+	public void cal();
+}
+
+class Squre implements Printable,Calculate // implements from printable and calculate interface 
+{    int a,s;
+	 public void cal()
+	 {
+		 Scanner sc = new Scanner(System.in);
+		 System.out.println("Enter Number of finding Squre :");
+		  a= sc.nextInt();
+		  s= a*a;                      
+	 }
+	 public void print()
+	 {   
+		 System.out.println("Squre of "+a+" is "+s);
+	 }
+}
+
+class Cube implements Printable, Calculate
+{   
+	int a,s;
+	public void cal()
+	{ 
+		 Scanner sc = new Scanner(System.in);
+		 
+		 System.out.println("Enter Number of finding Cube :");
+		 a= sc.nextInt();
+		 
+		 s= a*a*a;
+	}
+	public void print()
+	{
+		System.out.println("Cube of "+a+" is "+s);
+	}
+}
+
+public class FourthAss
+{
+   public static void main(String[] args)
+   {
+	   Squre d= new Squre();
+	   d.cal();
+	   d.print();
+	   
+	   Cube c= new Cube();
+	   c.cal();
+	   c.print();
+			   
+   }
+}
